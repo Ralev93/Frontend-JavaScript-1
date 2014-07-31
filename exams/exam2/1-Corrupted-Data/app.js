c9ecc92868ff3abb6e7e571749f1a45f83c24a86
Hristo Ralev <hbr@abv.bg>
2014-07-31 15:04:44 +0300

    edit

--- exams/exam2/1-Corrupted-Data/app.js
+++ exams/exam2/1-Corrupted-Data/app.js
@@ -2,9 +2,14 @@
 
 var data = require("./data");
 var helper = {};
+
 data.forEach(function (c) {
-  !helper[c["fields"]["date"]] ? helper[c["fields"]["date"]] = {} : {}; // {} = do nothing
-  helper[c["fields"]["date"]][c["fields"]["student"]] ? console.log(c) : helper[c["fields"]["date"]][c["fields"]["student"]] = 1;
+  var
+    date    = c["fields"]["date"],
+    student = c["fields"]["student"];
+
+  !helper[date] ? helper[date] = {} : {}
+  helper[date][student] ?  console.log(c) :  helper[date][student] = 1;
 });
 //time:   O(n)
 //memory: O(n)
